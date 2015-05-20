feffefa8437193829d6692f262fd724f0b837d80
robert <robert@roberts-Mac-Pro.local>
2015-05-21 00:34:42 +1000

    show example code to list hdfs files

--- src/main/scala/robco/Main.scala
+++ src/main/scala/robco/Main.scala
@@ -4,6 +4,21 @@ package robco
  * Created by robert on 11/05/2015
  */
 
+import java.net.URI
+
+import org.apache.hadoop.conf._
+import org.apache.hadoop.fs._
+
+object HDFSFileService {
+  val conf = new Configuration()
+  val fileSystem = FileSystem.get(URI.create("hdfs://52.64.112.251:9000/"), conf)
+}
+
 object Main extends App {
-  println("testing")
+  val fs = HDFSFileService.fileSystem
+  val iterator = fs.listFiles(new Path("/"), true)
+  while (iterator.hasNext) {
+    println(iterator.next.getPath)
+  }
+//  println("testing")
 }
