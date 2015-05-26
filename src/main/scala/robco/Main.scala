a52d1cbe3ab8153aadb31388a21b694f1929094b
amazoncop <nbn@nbns-MacBook-Pro.local>
2015-05-26 21:06:55 +1000

    replace extends App with def main()

--- src/main/scala/robco/Main.scala
+++ src/main/scala/robco/Main.scala
@@ -8,25 +8,25 @@ import java.net.URI
 import org.apache.hadoop.conf._
 import org.apache.hadoop.fs._
 
-object HDFSFileService {
-  val conf = new Configuration()
-  val fileSystem = FileSystem.get(URI.create("hdfs://localhost:9000/"), conf)
-}
+object Main {
+  def main(args: Array[String]) = {
+    val conf = new Configuration()
+    val hdfsRoot = args(0)
+    val fileSystem = FileSystem.get(URI.create(hdfsRoot), conf)
+    val fs = fileSystem
+    val iterator = fs.listFiles(new Path("/"), true)
 
-object Main extends App {
-  val fs = HDFSFileService.fileSystem
-  val iterator = fs.listFiles(new Path("/"), true)
+    println("Files:")
+    while (iterator.hasNext) { println(iterator.next.getPath.getName) }
 
-  println("Files:")
-  while (iterator.hasNext) { println(iterator.next.getPath.getName) }
+    val src = new Path("/hosts")
+    fs.delete(src, true)
 
-  val src = new Path("/hosts")
-  fs.delete(src, true)
+    println("Files:")
+    while (iterator.hasNext) { println(iterator.next.getPath.getName) }
+    fs.copyFromLocalFile(new Path("/etc/hosts"), new Path("/hosts"))
 
-  println("Files:")
-  while (iterator.hasNext) { println(iterator.next.getPath.getName) }
-  fs.copyFromLocalFile(new Path("/etc/hosts"), new Path("/hosts"))
-
-  println("Files:")
-  while (iterator.hasNext) { println(iterator.next.getPath.getName) }
+    println("Files:")
+    while (iterator.hasNext) { println(iterator.next.getPath.getName) }
+  }
 }
