865c46d0e5e1c107426be150846d1013d79f4bed
EC2 Default User <ec2-user@ip-172-31-1-192.ap-southeast-2.compute.internal>
2015-05-27 09:09:33 +0000

    allow upload-to-hdfs script to support different datasets

--- bin/test-data/upload-to-hdfs.sh
+++ bin/test-data/upload-to-hdfs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for file in data*.csv; do
+APP="app1"
+[ -n "$1" ] && APP="$1"
+
+for file in data*$APP*.csv; do
     echo $file
     /root/ephemeral-hdfs/bin/hdfs dfs -rm /$file
     /root/ephemeral-hdfs/bin/hdfs dfs -put $file /$file 
